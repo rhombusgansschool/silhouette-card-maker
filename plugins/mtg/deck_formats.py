cb8bd9358d4c909b825f0d7caa4c420020a32cc0
Alan-Cha <Alan.cha1@ibm.com>
2025-04-24 22:35:25 -0400

    Test different formats
    Signed-off-by: Alan-Cha <Alan.cha1@ibm.com>

--- plugins/mtg/deck_formats.py
+++ plugins/mtg/deck_formats.py
@@ -1,14 +1,18 @@
 import re
 
 from enum import Enum
-# from typing import List
 
-# A list of names
+# Isshin, Two Heavens as One
+# Arid Mesa
+# Battlefield Forge
+# Blazemire Verge
+# Blightstep Pathway
+# Blood Crypt
 def parse_simple_list(deck_text, handle_card):
     for line in deck_text.strip().split('\n'):
         name = line.strip()
         if name:
-            handle_card()(name)
+            handle_card(0, name)
 
 # About
 # Name Musashi's Mosh Pit [Primer]
@@ -22,7 +26,7 @@ def parse_simple_list(deck_text, handle_card):
 # 1x Blazemire Verge (DSK) 256
 # 1x Blightstep Pathway (KHM) 291
 # 1x Blood Crypt (RNA) 245
-def parse_mtga(deck_text):
+def parse_mtga(deck_text, handle_card):
     pattern = re.compile(r'(\d+)x?\s+(.+?)\s+\((\w+)\)\s+(\d+)', re.IGNORECASE)
     fallback_pattern = re.compile(r'(\d+)x?\s+(.+)')
 
@@ -37,14 +41,14 @@ def parse_mtga(deck_text):
             name = match.group(2).strip()
             set_code = match.group(3).strip()
             collector_number = match.group(4).strip()
-            handle_card(name, set_code, collector_number, quantity)
+            handle_card(0, name, set_code, collector_number, quantity)
         else:
             # Handle simpler "1x Mountain" lines
             fallback_match = fallback_pattern.match(line)
             if fallback_match:
                 quantity = int(fallback_match.group(1))
                 name = fallback_match.group(2).strip()
-                handle_card()(name, quantity=quantity)
+                handle_card(0, name)
 
 # 1 Abzan Battle Priest
 # 1 Abzan Falconer
@@ -60,7 +64,7 @@ def parse_mtgo(deck_text, handle_card):
         if len(parts) == 2:
             quantity = int(parts[0])
             name = parts[1].strip()
-            handle_card()(name, quantity=quantity)
+            handle_card(0, name)
 
 # 1x Agadeem's Awakening // Agadeem, the Undercrypt (znr) 90 [Resilience,Land]
 # 1x Ancient Cornucopia (big) 16 [Maybeboard{noDeck}{noPrice},Mana Advantage]
@@ -78,7 +82,7 @@ def parse_archidekt(deck_text, handle_card):
             name = match.group(2).strip()
             set_code = match.group(3).strip()
             collector_number = match.group(4).strip()
-            handle_card(name, set_code, collector_number, quantity)
+            handle_card(0, name, set_code, collector_number, quantity)
 
 # //Main
 # 1 [2XM#310] Ash Barrens
@@ -97,7 +101,7 @@ def parse_deckstats(deck_text, handle_card):
             collector_number = match.group(2)
             name = match.group(3).strip()
             quantity = int(line.split()[0])
-            handle_card(name, set_code, collector_number, quantity)
+            handle_card(0, name, set_code, collector_number, quantity)
 
 # 1 Lulu, Loyal Hollyphant (CLB) 477 *E*
 # 1 Abzan Battle Priest (IMA) 2
@@ -117,7 +121,7 @@ def parse_moxfield(deck_text, handle_card):
             collector_number = match.group(4).strip()
             print(f"Searching for {name}")
             print(name, set_code, collector_number, quantity)
-            handle_card(name, set_code, collector_number, quantity)
+            handle_card(0, name, set_code, collector_number, quantity)
 
 # moxfield_pattern = re.compile(r'^(\d+)\s+(.+?)\s+\((\w+)\)\s+([\w\-]+)')
 # def match_moxfield(deck_line: str):
@@ -154,7 +158,6 @@ class DeckFormat(str, Enum):
     DECKSTATS = "deckstats"
     MOXFIELD = "moxfield"
 
-
 def parse_deck(deck_text: str, format: DeckFormat, handle_card):
     # format_type = detect_format(deck_text)
     if format == DeckFormat.SIMPLE:
@@ -171,4 +174,3 @@ def parse_deck(deck_text: str, format: DeckFormat, handle_card):
         parse_moxfield(deck_text, handle_card)
     else:
         raise ValueError("Unrecognized deck format")
-
--- plugins/mtg/scryfall.py
+++ plugins/mtg/scryfall.py
@@ -6,196 +6,20 @@ import re
 import requests
 import time
 
-# User configurable options:
-
-csv_name = 'MOM_test.csv'
-
-# Use the printings of the following sets
-preferred_sets = []
-# preferred_sets = ["akh", "hou", "otj"]
-
-# Throw an error if there are not printings of the preferred sets
-enforce_preferred_sets = True
-
-# Prioritize older sets rather than newer sets
-prefer_older_sets = True
-
-# Add maybeboard in the list of cards
-add_maybeboard = False
-
-use_set_and_collector_number = True
-# use_set_and_collector_number = False
-
-
-
-# System options:
-
-# https://scryfall.com/docs/api
-query_headers = {'user-agent': 'silhouette-card-maker/0.1', 'accept': '*/*'}
-# card_info_query = 'https://api.scryfall.com/cards/named?exact='
-
-# card_info_columns = ['name', 'layout', 'Set', 'Collector Number', 'Quantity']
-# card_info = []
-
-
-
-# # Query all the card information from scryfall
-# csv_dir = 'mtg'
-# csv_path = os.path.join(csv_dir, csv_name)
-
-# counter = 0
-
-# with open(csv_path) as csvfile:
-#     cube_list = csv.DictReader(csvfile)
-
-#     # Iterate through all the cards
-#     for card in cube_list:
-#         card_name = card['name']
-
-#         if 'maybeboard' in card and card['maybeboard'] == 'true' and not add_maybeboard:
-#             print(f'skipping maybeboard card: "{card_name}"')
-#             continue
-
-#         card_quantity = 1
-#         if 'Quantity' in card:
-#             card_quantity = card['Quantity']
-
-#         print(f'{counter}: {card_name}')
-#         counter = counter + 1
-
-#         try:
-#             info = []
-
-#             if use_set_and_collector_number:
-#                 card_info_query_full = f"https://api.scryfall.com/cards/{card['Set']}/{card['Collector Number']}"
-
-#                 # Query the card using scryfall's API
-#                 r = requests.get(card_info_query_full, headers = query_headers)
-
-#                 # Sleep for 150 milliseconds, greater than the 100ms requested by scryfall API documentation
-#                 time.sleep(0.15)
-
-#                 # Check for 2XX response code
-#                 r.raise_for_status()
-
-#                 # Parse JSON
-#                 card_json = r.json()
-
-#                 card_info.append([card_name, card_json['layout'], card['Set'], card['Collector Number'], card_quantity])
-
-#             else:
-#                 # Filter out symbols from card names
-#                 card_name = re.sub(r'[^\w]', '', card_name)
-#                 card_info_query_full = card_info_query + card_name
-#                 # print(f"query: {card_info_query_full}")
-
-#                 # Query the card using scryfall's API
-#                 r = requests.get(card_info_query_full, headers = query_headers)
-
-#                 # Sleep for 150 milliseconds, greater than the 100ms requested by scryfall API documentation
-#                 time.sleep(0.15)
-
-#                 # Check for 2XX response code
-#                 r.raise_for_status()
-
-#                 # Parse JSON
-#                 card_json = r.json()
-
-#                 # Make request for all printings
-#                 prints_search_r = requests.get(card_json['prints_search_uri'])
-
-#                 # Sleep for 150 milliseconds, greater than the 100ms requested by scryfall API documentation
-#                 time.sleep(0.15)
-
-#                 # Check for 2XX response code
-#                 prints_search_r.raise_for_status()
-
-#                 # Parse JSON
-#                 prints_search_json = prints_search_r.json()
-
-#                 # Ensure there is at least one printing
-#                 card_printings = prints_search_json['data']
-#                 if len(card_printings) == 0:
-#                     raise Exception(f'cannot find printings for "{card_name}"')
-
-#                 # Remove promo, digital, and full art printings
-#                 clean_card_printings = list(filter(lambda card_print: card_print['lang'] == 'en' and card_print['nonfoil'] and not card_print['promo'] and not card_print['digital'] and not card_print['full_art'], card_printings))
-#                 if len(clean_card_printings) == 0:
-#                     raise Exception(f'after cleaning, no acceptable printings for "{card_name}"')
-
-#                 # Flip the order of the list if older printings are preferred
-#                 if prefer_older_sets:
-#                     clean_card_printings.reverse()
-
-#                 # If there are no preferred sets, then just select the first appropriate card printing
-#                 if len(preferred_sets) == 0:
-#                     card_print = clean_card_printings[0]
-#                     card_info.append([card_name, card_print['layout'], card_print['set'], card_print['collector_number'], card_quantity])
-
-#                 else:
-#                     added_card = False
-#                     for card_print in clean_card_printings:
-#                         if card_print['set'] in preferred_sets:
-#                             added_card = True
-#                             card_info.append([card_name, card_print['layout'], card_print['set'], card_print['collector_number'], card_quantity])
-#                             break
-
-#                     # Check if a printing has been selected
-#                     if not added_card:
-#                         if enforce_preferred_sets:
-#                             raise Exception(f'cannot find a print for "{card_name}" in the sets {preferred_sets}')
-#                         else:
-#                             print(f'no preferred printing for "{card_name}", defaulting to random printing')
-#                             card_print = clean_card_printings[0]
-#                             card_info.append([card_name, card_print['layout'], card_print['set'], card_print['collector_number'], card_quantity])
-
-#             # if 'Quantity' in card:
-#             #     for i in range(int(card['Quantity'])):
-#             #         card_info.append(info)
-
-#             # else:
-#             #     card_info.append(info)
-
-#         except requests.HTTPError as e:
-#             raise e
-#         except KeyError as e:
-#             print(f'Key error for card "{card_name}"')
-#             raise e
-#             # print(card_json)
-
-# # Convert card_info into a DataFrame
-# card_df = pd.DataFrame(card_info, columns = card_info_columns)
-
-# # Save as a pickled object
-# export_path = os.path.join(csv_dir, csv_name.split('.')[0]+'.pkl')
-# card_df.to_pickle(export_path)
-
-query_headers = {'user-agent': 'silhouette-card-maker/0.1', 'accept': '*/*'}
-card_image_query_1 = 'https://api.scryfall.com/cards'
-card_image_query_2 = '?format=image&version=large'
-
 double_sided_layouts = ['transform', 'modal_dfc']
 
-def request_art(
+def request_scryfall(
     query: str,
-    # image_path: str
 ):
-    try:
-        r = requests.get(query, headers = query_headers)
-        # Check for 2XX response code
+    r = requests.get(query, headers = {'user-agent': 'silhouette-card-maker/0.1', 'accept': '*/*'})
+    
+    # Check for 2XX response code
+    r.raise_for_status()
 
-        r.raise_for_status()
+    # Sleep for 150 milliseconds, greater than the 100ms requested by scryfall API documentation
+    time.sleep(0.15)
 
-        # for counter in range(card_quantity):
-        #     with open(image_path, 'wb') as f:
-        #         f.write(r.content)
-
-        return r.content
-
-        # Sleep for 150 milliseconds, greater than the 100ms requested by scryfall API documentation
-        time.sleep(0.15)
-    except requests.HTTPError as ex:
-        raise ex
+    return r
 
 def fetch_card_art(
     index: int,
@@ -209,80 +33,27 @@ def fetch_card_art(
     front_img_dir: str,
     double_sided_dir: str
 ):
-
-
     # Query for the front side
-    # card_image_query_front = '/'.join([card_image_query_1, card_set, card_num]) + card_image_query_2
-    # card_image_query_front = f'{card_image_query_1}/{card_set}/{card_num}/{card_image_query_2}'
-    card_image_query_front = f'https://api.scryfall.com/cards/{card_set}/{card_collector_number}/?format=image&version=large'
-
-
-    # image_path = os.path.join(front_img_dir, f'{str(index)}{clean_card_name}{str(counter)}.jpg')
-    card_art = request_art(card_image_query_front)
+    card_front_image_query = f'https://api.scryfall.com/cards/{card_set}/{card_collector_number}/?format=image&version=large'
+    card_art = request_scryfall(card_front_image_query).content
     if card_art is not None:
+
+        # Save image based on quantity
         for counter in range(quantity):
             image_path = os.path.join(front_img_dir, f'{str(index)}{clean_card_name}{str(counter)}.jpg')
 
             with open(image_path, 'wb') as f:
                 f.write(card_art)
 
-    # try:
-    #     # r = requests.get(card_image_query_front, headers = query_headers)
-    #     # # Check for 2XX response code
-
-    #     # r.raise_for_status()
-
-    #     # for counter in range(card_quantity):
-    #     #     image_path = os.path.join(front_img_dir, f'{str(index)}{clean_card_name}{str(counter)}a.jpg')
-    #     #     with open(image_path, 'wb') as f:
-    #     #         f.write(r.content)
-
-    #     # # Sleep for 150 milliseconds, greater than the 100ms requested by scryfall API documentation
-    #     # time.sleep(0.15)
-
-
-
-
-
-    #     for counter in range(card_quantity):
-    #         image_path = os.path.join(front_img_dir, f'{str(index)}{clean_card_name}{str(counter)}.jpg')
-
-    #         with open(image_path, 'wb') as f:
-    #             request_art(card_image_query_front)
-
-    # except requests.HTTPError as ex:
-    #     raise ex
-
+    # Get backside of card, if it exists
     if layout in double_sided_layouts:
-        # # Query for the back side
-        # try:
-        #     r = requests.get(f'{card_image_query_front}&face=back', headers = query_headers)
-
-        #     # Check for 2XX response code
-        #     r.raise_for_status()
-
-        #     for counter in range(card_quantity):
-        #         image_path = os.path.join(double_sided_dir, f'{str(index)}{clean_card_name}{str(counter)}.jpg')
-        #         with open(image_path, 'wb') as f:
-        #             f.write(r.content)
-
-        #     # Sleep for 150 milliseconds, greater than the 100ms requested by scryfall API documentation
-        #     time.sleep(0.15)
-        # except requests.HTTPError as ex:
-        #     raise ex
-
-        # card_image_query_back = f'{card_image_query_front}&face=back'
-        # for counter in range(card_quantity):
-        #     image_path = os.path.join(double_sided_dir, f'{str(index)}{clean_card_name}{str(counter)}.jpg')
-
-        #     with open(image_path, 'wb') as f:
-        #         request_art(card_image_query_back)
-
-        card_image_query_back = f'{card_image_query_front}&face=back'
-        card_art = request_art(card_image_query_back)
+        card_back_image_query = f'{card_front_image_query}&face=back'
+        card_art = request_scryfall(card_back_image_query).content
         if card_art is not None:
+
+            # Save image based on quantity
             for counter in range(quantity):
-                image_path = os.path.join(front_img_dir, f'{str(index)}{clean_card_name}{str(counter)}.jpg')
+                image_path = os.path.join(double_sided_dir, f'{str(index)}{clean_card_name}{str(counter)}.jpg')
 
                 with open(image_path, 'wb') as f:
                     f.write(card_art)
@@ -308,19 +79,10 @@ def fetch_card(
     double_sided_dir: str
 ):
     if use_set_and_collector_number and card_set != "" and card_collector_number != "":
-        card_info_query_full = f"https://api.scryfall.com/cards/{card_set}/{card_collector_number}"
+        card_info_query = f"https://api.scryfall.com/cards/{card_set}/{card_collector_number}"
 
-        # Query the card using scryfall's API
-        r = requests.get(card_info_query_full, headers = query_headers)
-
-        # Sleep for 150 milliseconds, greater than the 100ms requested by scryfall API documentation
-        time.sleep(0.15)
-
-        # Check for 2XX response code
-        r.raise_for_status()
-
-        # Parse JSON
-        card_json = r.json()
+        # Query for card info
+        card_json = request_scryfall(card_info_query).json()
 
         fetch_card_art(index, quantity, remove_nonalphanumeric(card_json['name']), card_set, card_collector_number, card_json['layout'], front_img_dir, double_sided_dir)
 
@@ -330,36 +92,15 @@ def fetch_card(
 
         # Filter out symbols from card names
         clear_card_name = remove_nonalphanumeric(name)
-        card_info_query_full = f'https://api.scryfall.com/cards/named?exact={clear_card_name}'
 
-        # Query the card using scryfall's API
-        r = requests.get(card_info_query_full, headers = query_headers)
+        card_info_query = f'https://api.scryfall.com/cards/named?exact={clear_card_name}'
 
-        # Sleep for 150 milliseconds, greater than the 100ms requested by scryfall API documentation
-        time.sleep(0.15)
+        # Query for card info
+        card_json = request_scryfall(card_info_query).json()
 
-        # Check for 2XX response code
-        r.raise_for_status()
-
-        # Parse JSON
-        card_json = r.json()
-
-        # Make request for all printings
-        prints_search_r = requests.get(card_json['prints_search_uri'])
-
-        # Sleep for 150 milliseconds, greater than the 100ms requested by scryfall API documentation
-        time.sleep(0.15)
-
-        # Check for 2XX response code
-        prints_search_r.raise_for_status()
-
-        # Parse JSON
-        prints_search_json = prints_search_r.json()
-
-        # Ensure there is at least one printing
+        # Get available printings
+        prints_search_json = request_scryfall(card_json['prints_search_uri']).json()
         card_printings = prints_search_json['data']
-        # if len(card_printings) == 0:
-        #     raise Exception(f'cannot find printings for "{name}"')
 
         # Remove promo, digital, and full art printings
         filtered_card_printings = list(filter(lambda card_print: card_print['lang'] == 'en' and card_print['nonfoil'] and not card_print['promo'] and not card_print['digital'] and not card_print['full_art'], card_printings))
