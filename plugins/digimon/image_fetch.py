d5405f977bcd31c0589fd02dc3d129cd4e23e2a0
ThisJustZack <34222941+ThisJustZack@users.noreply.github.com>
2025-08-10 15:22:39 -0400

    Modify to card art retrieval
    Modify to use Bandai URL
    
    Modify URL to be template

--- plugins/digimon/image_fetch.py
+++ plugins/digimon/image_fetch.py
@@ -2,7 +2,9 @@ from os import path
 from requests import Response, get
 from time import sleep
 
-def request_digimoncard(query: str) -> Response:
+CARD_ART_URL_TEMPLATE = 'https://world.digimoncard.com/images/cardlist/card/{card_number}.png'
+
+def request_digimon(query: str) -> Response:
     r = get(query, headers = {'user-agent': 'silhouette-card-maker/0.1', 'accept': '*/*'})
 
     r.raise_for_status()
@@ -12,7 +14,7 @@ def request_digimoncard(query: str) -> Response:
 
 def fetch_card_art(index: int, card_number: str, quantity: int, front_img_dir: str):
 
-    card_art = request_digimoncard(f'https://images.digimoncard.io/images/cards/{card_number}.jpg').content
+    card_art = request_digimon(CARD_ART_URL_TEMPLATE.format(card_number=card_number)).content
     
     if card_art is not None:
         # Save image based on quantity
