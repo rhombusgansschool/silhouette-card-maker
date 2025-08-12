e16d513e1b9299e7673a532e7cc9c848ac6596e2
ThisJustZack <34222941+ThisJustZack@users.noreply.github.com>
2025-08-11 21:46:43 -0400

    Refactor for file output template
    Make output template modular

--- plugins/one_piece/one_piece.py
+++ plugins/one_piece/one_piece.py
@@ -4,6 +4,8 @@ from time import sleep
 
 CARD_ART_URL_TEMPLATE = 'https://en.onepiece-cardgame.com/images/cardlist/card/{card_number}.png'
 
+OUTPUT_CARD_ART_FILE_TEMPLATE = '{deck_index}{card_number}{quantity_counter}.png'
+
 def request_bandai(query: str) -> Response:
     r = get(query, headers = {'user-agent': 'silhouette-card-maker/0.1', 'accept': '*/*'})
 
@@ -21,11 +23,14 @@ def fetch_card(
     # Query for card art
     card_art = request_bandai(CARD_ART_URL_TEMPLATE.format(card_number=card_number)).content
 
-    for counter in range(quantity):
-        image_path = path.join(front_img_dir, f'{str(index)}{card_number}{str(counter + 1)}.png')
+    if card_art is not None:
 
-        with open(image_path, 'wb') as f:
-            f.write(card_art)
+        # Save image based on quantity
+        for counter in range(quantity):
+            image_path = path.join(front_img_dir, OUTPUT_CARD_ART_FILE_TEMPLATE.format(deck_index=str(index), card_number=card_number, quantity_counter=str(counter+1)))
+
+            with open(image_path, 'wb') as f:
+                f.write(card_art)
 
 def get_handle_card(
     front_img_dir: str,
