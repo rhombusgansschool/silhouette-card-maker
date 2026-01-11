449eb11dc600207160b9c254166e532eb2762318
Alan Cha <ac3805@columbia.edu>
2026-01-10 23:55:17 -0500

    Reduce mime types
    Signed-off-by: Alan Cha <ac3805@columbia.edu>

--- utilities.py
+++ utilities.py
@@ -22,23 +22,24 @@ layouts_path = os.path.join(asset_directory, layouts_filename)
 
 # Specify valid mimetypes for images
 # List can be found here: https://github.com/h2non/filetype.py?tab=readme-ov-file#image
+# Pillow suported formats: https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
 valid_mimetypes = (
-    "image/vnd.dwg",
-    "image/x-xcf",
+    # "image/vnd.dwg",
+    # "image/x-xcf",
     "image/jpeg",
     "image/jpx",
-    "image/jxl",
+    # "image/jxl",
     "image/png",
     "image/apng",
     "image/gif",
     "image/webp",
-    "image/x-canon-cr2",
+    # "image/x-canon-cr2",
     "image/tiff",
     "image/bmp",
-    "image/vnd.ms-photo",
-    "image/vnd.adobe.photoshop",
-    "image/x-icon",
-    "image/heic",
+    # "image/vnd.ms-photo",
+    # "image/vnd.adobe.photoshop",
+    # "image/x-icon",
+    # "image/heic",
     "image/avif",
     "image/qoi",
     "image/dds"
